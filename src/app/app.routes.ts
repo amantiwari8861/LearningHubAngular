54c3be876194d6e78844ce271b29b348492660a0
Aman Tiwari <amantiwari8861@gmail.com>
2025-06-07 18:59:26 +0530

    tailwind,Router in UI set Up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,48 @@
 import { Routes } from '@angular/router';
+import { Landing } from './pages/landing/landing';
+import { About } from './pages/about/about';
+import { Contact } from './pages/contact/contact';
+import { Notfound } from './pages/notfound/notfound';
+import { Explore } from './pages/explore/explore';
+import { Login } from './pages/login/login';
+import { Register } from './pages/register/register';
+import { Dashboard } from './pages/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: Landing
+    },
+    {
+        path: "about",
+        component: About
+    },
+    {
+        path: "contact",
+        component: Contact
+    },
+    {
+        path: "explore",
+        component: Explore
+    },
+    {
+        path: "login",
+        component: Login
+    },
+    {
+        path: "register",
+        component: Register
+    },
+    {
+        path: "dashboard",
+        component: Dashboard
+    },
+    {
+        path: "contact",
+        component: Contact
+    },
+    {
+        path: "**",
+        component: Notfound
+    }
+];
